0caaf332142d3ad059a8df1a061a7deada4b4db0
Hariharan Selvaraj <hariharan.2309.s@icloud.com>
2024-09-11 14:20:16 +0200

    setting up mongo and mongoose

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,16 @@
-import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
-import { UsersModule } from './users/users.module';
+import { Module } from "@nestjs/common";
+import { AppController } from "./app.controller";
+import { AppService } from "./app.service";
+import { UsersModule } from "./users/users.module";
+import { MongooseModule } from "@nestjs/mongoose";
 
 @Module({
-  imports: [UsersModule],
+  imports: [
+    UsersModule,
+    MongooseModule.forRoot(
+      "mongodb+srv://threads-user:snahmldY6lKYLlv3@cluster0.efzcccl.mongodb.net/threads?retryWrites=true&w=majority&appName=Cluster0"
+    ),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
